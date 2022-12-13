7c9dbfae5b624779dc1fff1f301ed0dcd7527f78
Thomas Froger <thomasfroger1@gmail.com>
2022-12-13 11:01:35 +0100

    exo 2 test ?

---
+++ exo2-cyphersuite.py
@@ -0,0 +1,16 @@
+from string import maketrans
+
+#----------------------
+def cipher():
+    rot13trans = maketrans('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
+                       'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm')
+    def rot13(text):
+        return text.translate(rot13trans)
+    def main():
+        txt = "ROT13 Algorithm"
+        print
+        rot13(txt)
+    if __name__ == "__main__":
+        main()
+
+#----------------------------------
