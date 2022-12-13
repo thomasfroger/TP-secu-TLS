8132b3643deed1e30340a5768f7ce003a37fa273
Raphael B <r.bidault@ecole-ipssi.net>
2022-12-13 10:07:54 +0100

    server/client

--- exo1-client.py
+++ exo1-client.py
@@ -1,25 +1,24 @@
-from pynput import keyboard
-import logging
-import time
 import socket
 
+def client_program():
+    host = socket.gethostname()  # as both code is running on same pc
+    port = 5000  # socket server port number
 
-host = socket.gethostname()  # as both code is running on same pc
-port = 5500  # socket server port number
+    client_socket = socket.socket()  # instantiate
+    client_socket.connect((host, port))  # connect to the server
 
-client_socket = socket.socket()  # instantiate
-client_socket.connect((host, port))  # connect to the server
-timer="( "+time.strftime("%D %H:%M:%S")+" ) "
-print("---  ---")
+    message = input(" Message : ")  # take input
 
-def on_press(key):
-    try:
-        message=timer+"Press : "+ str(key)
-        client_socket.send(message.encode())
-    except AttributeError:
-        client_socket.send(message.encode())
+    while message.lower().strip() != 'bye':
+        client_socket.send(message.encode())  # send message
+        data = client_socket.recv(1024).decode()  # receive response
 
-with keyboard.Listener(on_press=on_press) as listener:
-    listener.join()
+        print('Received from server: ' + data)  # show in terminal
 
-client_socket.close()  # close the connection
+        message = input(" -> ")  # again take input
+
+    client_socket.close()  # close the connection
+
+
+if __name__ == '__main__':
+    client_program()
--- exo1-server.py
+++ exo1-server.py
@@ -1,25 +1,31 @@
 import socket
 
-# get the hostname
-host = socket.gethostname()
-port = 5500  # initiate port no above 1024
 
-server_socket = socket.socket()  # get instance
-# look closely. The bind() function takes tuple as argument
-server_socket.bind((host, port))  # bind host address and port together
+def server_program():
+    # get the hostname
+    host = socket.gethostname()
+    port = 5000  # initiate port no above 1024
 
-# configure how many client the server can listen simultaneously
-server_socket.listen(2)
-conn, address = server_socket.accept()  # accept new connection
-print("Connection from: " + str(address))
-while True:
-    # receive data stream. it won't accept data packet greater than 1024 bytes
-    data = conn.recv(1024).decode()
-    #if not data:
-    #    print("no data")
-    #    break
-    print(str(data))
-    #data = input(' -> ')
-    #conn.send(data.encode())  # send data to the client
+    server_socket = socket.socket()  # get instance
+    # look closely. The bind() function takes tuple as argument
+    server_socket.bind((host, port))  # bind host address and port together
 
-conn.close()  # close the connection
+    # configure how many client the server can listen simultaneously
+    server_socket.listen(2)
+    conn, address = server_socket.accept()  # accept new connection
+    print("Connection from: " + str(address))
+    while True:
+        # receive data stream. it won't accept data packet greater than 1024 bytes
+        data = conn.recv(1024).decode()
+        if not data:
+            # if data is not received break
+            break
+        print("from connected user: " + str(data))
+        data = input(' -> ')
+        conn.send(data.encode())  # send data to the client
+
+    conn.close()  # close the connection
+
+
+if __name__ == '__main__':
+    server_program()
